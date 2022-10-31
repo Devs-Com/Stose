6d432a5908a58a70262af0dfb83ff1df4a1c7a5a
Anderson Mendoza <davidmramos53@gmail.com>
2022-10-30 19:43:29 -0500

    insertando modelo MVC con Spring Boot

---
+++ src/main/java/com/example/stose/StoseApplication.java
@@ -0,0 +1,13 @@
+package com.example.stose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoseApplication.class, args);
+	}
+
+}
