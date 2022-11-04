82f14c0f2098ba1f43032845feb899e9a53b960d
Anderson Mendoza <davidmramos53@gmail.com>
2022-11-04 17:14:03 -0500

    insertando controller + enlaces primordiales en carrito-compras

--- src/main/java/com/example/stose/controller/carritoComprasController.java
+++ src/main/java/com/example/stose/controller/carritoComprasController.java
@@ -1,5 +1,17 @@
 package com.example.stose.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+@RequestMapping("/inicio")
 public class carritoComprasController {
     
+    @GetMapping({ "/carrito-compras", "Carrito-Compras" })
+    public String ControllerCarritoCompras(Model model) {
+        return "carrito-compras";
+    }
 }
