0d925d2b6d42f19af50724668a36236fe5b31f21
Anderson Mendoza <davidmramos53@gmail.com>
2022-11-04 17:23:51 -0500

    insertando controller + enlaces primordiales en detalle-libro

--- src/main/java/com/example/stose/controller/detalleLibroController.java
+++ src/main/java/com/example/stose/controller/detalleLibroController.java
@@ -1,5 +1,17 @@
 package com.example.stose.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+@RequestMapping("/inicio")
 public class detalleLibroController {
     
+    @GetMapping({ "/detalle-libro", "Detalle-Libro" })
+    public String ControllerDetalleLibro(Model model) {
+        return "detalle-libro";
+    }
 }
