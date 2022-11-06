96556eeb352661a180d1514b11dcdbe9b7db2eae
yeshuaaguilar <yeshunator.12@gmail.com>
2022-11-06 13:02:09 -0500

    version 1.4

--- src/main/java/com/example/stose/models/BlogDTO.java
+++ src/main/java/com/example/stose/models/BlogDTO.java
@@ -1,17 +1,83 @@
 package com.example.stose.models;
 
-import java.time.LocalDate;
+//import java.time.LocalDate;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Blogs")
+@Document(collection = "products")
 public class BlogDTO {
 	
 	@Id
 	private String _id;
 	
-	private String name;
+	private String titulo;
+	
+	private String image;
+	
+	private String descripcion;
+	
+	private String minidescripcion;
+	
+	private String subtitulo;
+	
+	private String descripcion2;
+
+	public String get_id() {
+		return _id;
+	}
+
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getMinidescripcion() {
+		return minidescripcion;
+	}
+
+	public void setMinidescripcion(String minidescripcion) {
+		this.minidescripcion = minidescripcion;
+	}
+
+	public String getSubtitulo() {
+		return subtitulo;
+	}
+
+	public void setSubtitulo(String subtitulo) {
+		this.subtitulo = subtitulo;
+	}
+
+	public String getDescripcion2() {
+		return descripcion2;
+	}
+
+	public void setDescripcion2(String descripcion2) {
+		this.descripcion2 = descripcion2;
+	}
 
 	
 }
