c8c5c2a732a00cc150141d666a088bd67daf3d3b
Christofer66 <74540558@certus.edu.pe>
2022-12-10 15:42:45 -0500

    .

--- src/main/java/com/example/stose/seguridad/SecurityConfiguration.java
+++ src/main/java/com/example/stose/seguridad/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.example.stose.services.UsuarioServicio;
