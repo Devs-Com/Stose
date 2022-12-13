bd1498d90e85681d21e277a23266e0220be219dd
Jhon Franco Aguilar Rodriguez <73808133@certus.edu.pe>
2022-12-12 19:50:32 -0500

    Agregando formulario de admin en la vista Libro

--- src/main/resources/static/js/modal-admin.js
+++ src/main/resources/static/js/modal-admin.js
@@ -1,4 +1,8 @@
+
 //show modal on load
 function AbrirModal3() {
     document.querySelector('.site').classList.toggle('showmodal3')
 }
+document.querySelector('.modalclose3').addEventListener('click', function () {
+    document.querySelector('.site').classList.remove('showmodal3')
+})
