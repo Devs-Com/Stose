6165086212c1fb21f81cc531a139a1e10ebd5208
Christofer66 <74540558@certus.edu.pe>
2022-12-17 06:18:29 -0500

    Cambiando enlaces.

--- src/main/java/com/example/stose/seguridad/SecurityConfiguration.java
+++ src/main/java/com/example/stose/seguridad/SecurityConfiguration.java
@@ -44,6 +44,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(
+				"/inicio/**",
 				"/**",
 				"/js/**",
 				"/css/**",
